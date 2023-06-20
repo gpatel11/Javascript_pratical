f7fd331eb6b478296090d38004fabd2f0b351f25
rushitab115 <moradiyarushita61@gmail.com>
2023-06-20 18:31:18 +0530

    Fectorial

---
+++ script.js
@@ -0,0 +1,32 @@
+let a=1;
+let b=5;
+console.log(a+b);
+
+let Name="Rushita";
+let Surname="Moradiya";
+console.log(Name+" "+Surname)
+
+let x="hello";
+let y=45;
+let z=true;
+let A
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(A);
+
+
+
+// *******************************
+
+let i;
+let sum=1;
+let n=4;
+
+for(i=1;i<=n;i++){
+    sum=sum*i;
+}
+console.log(sum);
+
+
