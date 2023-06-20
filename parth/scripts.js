c935c1fd5a2a121f20cc942d2601cf67f698a4e8
parthg11111 <pgajera028@gmail.com>
2023-06-20 18:31:25 +0530

    js file fibonachi program

---
+++ parth/scripts.js
@@ -0,0 +1,19 @@
+let a=3;
+let b=6;
+console.log(a+b);
+
+let = 1545;
+console.log(let); 
+
+let name ="parth";
+let surname ="gajera";
+console.log(name+surname)
+
+let username=5;
+let sum=1;
+
+for(i=1;i<=username;i++)
+{
+    sum=sum*i;
+}
+console.log(sum);
