f556a3c073851b4cc103bb2527b255c0fd8d62cd
bansi740 <bansitalaviya740@gmail.com>
2023-06-21 18:29:24 +0530

    armstrong number

--- bansi/test.js
+++ bansi/test.js
@@ -1,9 +1,23 @@
- let userinput=5;
- let num
+// program to check an Armstrong number of three digits
 
+let sum = 0;
+const number = prompt('Enter a three-digit positive integer: ');
 
- for (int=i=1.i<=n;i++)
- {
-    sum=sum*i;
- }
- console.log(sum)
+// create a temporary variable
+let temp = number;
+while (temp > 0) {
+    // finding the one's digit
+    let remainder = temp % 10;
+
+    sum += remainder * remainder * remainder;
+
+    // removing last digit from the number
+    temp = parseInt(temp / 10); // convert float into integer
+}
+// check the condition
+if (sum == number) {
+    document.write(`${number} is an Armstrong number`);
+}
+else {
+   document.write(`${number} is not an Armstrong number.`);
+}
