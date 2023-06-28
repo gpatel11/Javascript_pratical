67ab7f8ddcf2aaa4673595fe56177c827dc6cf4a
juhilmoradiya <juhilmoradiya10@email.com>
2023-06-28 19:23:10 +0530

    prectice

---
+++ JUHIL/JS/script.js
@@ -0,0 +1,31 @@
+let a=3;
+let b=6;
+console.log(a+b);
+
+let Name="JUHIl";
+let Surename="Moradiya";
+    console.log(Surename+" "+Name);
+
+
+//************************************ */
+
+let userinput=8;
+let sum=1;
+let i;
+    for(i=1;i<=userinput;i++)
+
+        {
+            sum=sum*i;
+        }
+console.log(sum);
+
+// **********************************
+
+let user=5;
+let sum1=1;
+ 
+        for(let a=user; a>0; a--)
+        {
+            sum=sum*a
+        }
+console.log(sum);
